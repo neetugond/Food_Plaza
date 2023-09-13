ad5af121001213c9d999e699ab1ac7fce200c454
neetugond <ngond92@gmail.com>
2023-09-14 00:47:45 +0530

    function component

--- App.js
+++ App.js
@@ -2,12 +2,24 @@ import React from "react";
 import  ReactDOM  from "react-dom/client";
 
 // 2. JSX => React.createElement => Object => HTML(DOM)
-const heading = (
+const Heading = ()=>(
     <h1 id="title" key="h1">
         Namaste React
     </h1>
 )
+
+
+// 3. Functional component 
+const HeaderComponent = () => {
+    return (
+        <div>
+            <Heading/> //component composition - using component inside another component
+            <h1>Namaste React Functional Component</h1>
+            <h2>This is a h2 tag</h2>
+        </div>
+    )
+}
+
 const root = ReactDOM.createRoot(document.getElementById('root'))
 
-root.render(heading)
-
+root.render(<HeaderComponent/>)
