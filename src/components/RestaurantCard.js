987f0074bf1578c43d49eed6a7a5a44e703706cc
neetugond <ngond92@gmail.com>
2023-09-21 22:23:05 +0530

    make component systematic and export import

--- src/components/RestaurantCard.js
+++ src/components/RestaurantCard.js
@@ -1,13 +1,22 @@
 import React from "react";
+import {IMG_CDN_URL} from '../constants'
 
 
 
-const RestaurantsCard = () => {
+const RestaurantCard = ({
+    name,
+    cuisines,
+    avgRating,
+    cloudinaryImageId
+}) => {
     return (
-        <>
-            <h1>hello</h1>
-        </>
+        <div className="card">
+            <img src={"https://res.cloudinary.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_508,h_320,c_fill/" + cloudinaryImageId} />
+            <h3>{name}</h3>
+            <h2>{avgRating}</h2>
+            <p>{cuisines}</p>
+        </div>
     )
-}
+};
 
-export default RestaurantsCard;
+export default RestaurantCard;
