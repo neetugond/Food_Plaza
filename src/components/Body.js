f5b42ded177c404ff7687d7f64a083bbc5301820
neetugond <ngond92@gmail.com>
2023-09-28 15:51:13 +0530

    data fetch

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,19 +1,44 @@
-import React, { useState } from "react";
+import { useState, useEffect } from "react";
 import { restaurantList } from "../constants";
 import RestaurantCard from "./RestaurantCard";
 
-
 function filterData(searchText, restaurants) {
-    const filterData = restaurants.filter((restaurant) => restaurant?.data?.name.toLowerCase().includes(searchText.toLowerCase())
+    const filterData = restaurants.filter((restaurant) =>
+        restaurant?.data?.name.toLowerCase().includes(searchText.toLowerCase())
     );
     return filterData;
 }
 // no key <<<<< index key << unique key
 const Body = () => {
-    const [restaurants, setRestaurants] = useState(restaurantList)
+    const [allRestaurants, setAllRestaurants] = useState([])
+    const [filterRestaurants, setFilterRestaurants] = useState([]);
     const [searchText, setSearchText] = useState("") //declaring react local variable searchText. setSearchText - function to update the variable
+    
+
+    // useEffect - when we want to first render the page than api and update the page
+    useEffect(() => {
+        getRestaurants();
+    }, [])
+
+    async function getRestaurants() {
+        const data = await fetch('https://www.swiggy.com/dapi/restaurants/list/v5?lat=12.9351929&lng=77.62448069999999&page_type=DESKTOP_WEB_LISTING');
+        const json = await data.json();
+        console.log("json", json)
+         
+        const dataFirst = json?.data?.cards[3]?.card?.card?.gridElements?.infoWithStyle.restaurants
+
+
+        const dataSecond =json?.data?.cards[5]?.card?.card?.gridElements?.infoWithStyle.restaurants
+
+        // config driven ui data that why ternary operator
+        let result = (dataFirst == undefined) ? dataSecond : dataFirst
+        
+        setAllRestaurants(result)
+        setFilterRestaurants(result);
+        console.log(result)
+    }
     return (
-        <>
+        <div className="body">
             <div className="searchbox">
                 <input type="text" className='search-input' placeholder='Search'
                     value={searchText}
@@ -22,21 +47,21 @@ const Body = () => {
                     }} />
                 <button className='search-btn' onClick={() => {
                     // need to filter the data
-                    const data = filterData(searchText, restaurants)
+                    const data = filterData(searchText, allRestaurants)
                     //update the state - restaurants
-                    setRestaurants(data);
+                    setFilterRestaurants(data);
                 }}>search </button>
             </div>
             <div className="restaurant-list">
                 {
-                    restaurants.map((restaurant) => {
-                        return (<RestaurantCard {...restaurant.data} key={restaurant.data.id} />
+                    filterRestaurants.map((restaurant) => {
+                        return (<RestaurantCard resData ={restaurant?.info} key={restaurant.info.id} />
 
                         )
                     })
                 }
             </div>
-        </>
+        </div>
 
     )
 }
--- src/components/RestaurantCard.js
+++ src/components/RestaurantCard.js
@@ -3,18 +3,18 @@ import {IMG_CDN_URL} from '../constants'
 
 
 
-const RestaurantCard = ({
-    name,
-    cuisines,
-    avgRating,
-    cloudinaryImageId
-}) => {
+const RestaurantCard = (props) => {
+    const { resData } = props;
+    const {
+name,cloudinaryImageId,avgRating,cuisines,costForTwo
+    } = resData
     return (
         <div className="card">
             <img src={IMG_CDN_URL + cloudinaryImageId} />
             <h3>{name}</h3>
+            <h3>{ costForTwo}</h3>
             <h2>{avgRating}</h2>
-            <p>{cuisines}</p>
+            <p>{cuisines.join(', ')}</p>
         </div>
     )
 };
