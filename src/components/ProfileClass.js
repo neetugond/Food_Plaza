39ea20dbda8866419dd8d6a5bf04af72e3230eef
neetugond <ngond92@gmail.com>
2023-10-09 14:24:38 +0530

    class

--- src/components/ProfileClass.js
+++ src/components/ProfileClass.js
@@ -1,11 +1,40 @@
 import React from 'react'
 
 class ProfileClass extends React.Component{
+    constructor(props) {
+        super(props);
+        //create state
+        this.state = {
+            count: 0,
+            count2: 2,
+        };
+        console.log("constructor") //1st call 
+    }
+    componentDidMount() {
+        //3rd call - after render this is call i.e why we make api call here
+        console.log('ComponentDidMount')
+    }
+    componentDidUpdate() {
+        console.log('ComponentDidUpdate') //when we make any changes we can make here ([ dependency array])
+    }
+    componentWillUnmount() {
+        console.log('componentWillUnmount')
+        // change from this component to another
+    }
     render() {
+        console.log('render') //2nd call
         return (
             <div>
                 <h1>{ this.props.name}</h1>
                 <h2>Profile Class Component </h2>
+                <p>{this.state.count}</p>
+                <button onClick={() => {
+                    // we do not mutate state directly
+                    // never do this.state = something
+                    this.setState({
+                        count: 1
+                    })
+                }}>+</button>
             </div>
         )
     }
--- src/components/ProfileFunctional.js
+++ src/components/ProfileFunctional.js
@@ -1,9 +1,17 @@
+import { useEffect, useState } from "react"
 
 const ProfileFunctional = (props) => {
+    const [count, setCount] = useState(0)
+    useEffect(() => {
+        // console.log('useEffect') //2nd call
+    },[]);
+    // console.log('render') //1st call
     return (
         <div>
-            <h1>{ props.name}</h1>
+            {/* <h1>{ props.name}</h1> */}
             <h2>Profile Functional Component</h2>
+            <p>{count}</p>
+            <button onClick={()=>setCount(count+1)}> +</button>
         </div>
     )
 }
