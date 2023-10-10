f908e4d3687bce04822090709b8b7d1500bf81f1
neetugond <ngond92@gmail.com>
2023-10-10 23:48:35 +0530

    clean event listener

--- src/utils/useOnline.js
+++ src/utils/useOnline.js
@@ -3,16 +3,26 @@ import { useEffect, useState } from "react";
 const useOnline = () => {
     // for managing state
     const [isOnline, setIsOnline] = useState(true);
-    
+
     // want to show only once
     useEffect(() => {
-        window.addEventListener('online', () => {
+        const handleOnline = () => {
             setIsOnline(true)
-        });
-        window.addEventListener('offline', () => {
+        }
+
+        const handleOffline = () => {
             setIsOnline(false)
-        }); 
-    },[])
+        }
+
+        window.addEventListener('online', handleOnline);
+        window.addEventListener('offline', handleOffline);
+        // when we go to any another page than we should clear the event listener otherwise browser keep holding event listener and it can blow up.
+        return () => {
+            window.removeEventListener('online', handleOnline);
+
+            window.removeEventListener('offline', handleOffline)
+        }
+    }, [])
     //return boolean true and false
     return isOnline;
 }
